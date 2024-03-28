8c43dd810f76a1d50a41a2e61401c12f97c7fe45
workwithhassan <workwithhassan021@gmail.com>
2024-03-28 17:02:05 +0500

    validation form

---
+++ form.js
@@ -0,0 +1,43 @@
+let emailInput = document.getElementById('email');
+let passwordInput = document.getElementById('password');
+let ageInput = document.getElementById('age');
+let form = document.getElementById('form');
+
+form.addEventListener('submit', function (event) {
+    event.preventDefault();
+
+    validateInputs();
+});
+
+let errors = {
+    isEmailValid: false,
+    isPasswordValid: false,
+    isAgeValid: false
+}
+
+function validateInputs() {
+    let email = emailInput.value;
+    let password = passwordInput.value;
+    let age = ageInput.value;
+
+    if (email === '') {
+        errors.isEmailValid = false;
+        emailError.innerHTML = 'Email is required';
+    } else {
+        errors.isEmailValid = true;
+    }
+
+    if (password === '') {
+        errors.isPasswordValid = false;
+        passwordError.innerHTML = 'Password is required';
+    } else {
+        errors.isPasswordValid = true;
+    }
+
+    if (age === '') {
+        errors.isAgeValid = false;
+        ageError.innerHTML = 'Age is required';
+    } else {
+        errors.isAgeValid = true;
+    }
+}
